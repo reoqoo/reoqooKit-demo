74399614a6e50f50ff692e5f137f2768fe6c462f
xiaojuntao <xiaojuntao@gwell.cc>
2024-10-17 10:22:04 +0800

    移除了消息中心的`福利中心`页面
    (cherry picked from commit fe7527de74c085fab198e48b90fe5d297608821e)

--- RQSDKDemo/Classes/AppEntrance/KeyWindow.swift
+++ RQSDKDemo/Classes/AppEntrance/KeyWindow.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-/// 负责 摇一摇 弹出 debug 助手功能
+/// ~~负责 摇一摇 弹出 debug 助手功能~~
 /// 在多屏幕需求出现之前, 此 Window 实例仅有一份, 且生命周期由 AppDelegate 或 SceneDelegate 管理, AppEntranceManager 也会持有此 Window 实例, 掌握生命周期
 class KeyWindow: UIWindow {
 

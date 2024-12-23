c48a0aebb7ced588b22c08f6edcdb408cf74f003
xiaojuntao <xiaojuntao@gwell.cc>
2024-12-23 16:38:35 +0800

    optimize: Our SDK did mark the RQCore.Localization as a private level, so the way to getting localize strings did update. please follow up this changed
    (cherry picked from commit 3508ae780c57aaf4a4205f5abd42edf07877e716)

--- RQSDKDemo/Localization.swift
+++ RQSDKDemo/Localization.swift
@@ -8,7 +8,40 @@
 import Foundation
 
 extension String {
-    static var localization: String.Localization = {
+
+    public class Localization {
+
+        let bundle: Bundle
+        /// which bundle's localization files want load
+        init(bundle: Bundle) {
+            self.bundle = bundle
+        }
+
+        lazy var englishLocalizationFile = Bundle.init(path: self.bundle.path(forResource: "en", ofType: "lproj")!)
+
+        /// 语言国际化
+        ///
+        ///     Localized("AA0111", note: "密码错误")
+        ///
+        /// - Parameters:
+        ///   - key: 键值
+        ///   - note: 注释信息，为便于阅读代码请传入中文注释
+        ///   - args: 字符串中要替换的参数值, 例如: "My name is %@, i'm %ld years old"
+        /// - Returns: 翻译文案
+        public func localized(_ key: String, note: String, args: String...) -> String {
+            var translation = NSLocalizedString(key, comment: note)
+            if translation.isEmpty || translation == key || translation == "null" {
+                translation = self.englishLocalizationFile?.localizedString(forKey: key, value: nil, table: "Localizable") ?? note
+                if translation.isEmpty || translation == key || translation == "null" {
+                    translation = note
+                }
+            }
+            translation = String(format: translation, arguments: args)
+            return translation
+        }
+    }
+
+    static var localization: Localization = {
         return .init(bundle: Bundle.main)
     }()
 }
